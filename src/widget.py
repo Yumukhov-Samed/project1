3e443f5a334cf05b4a68d72a0780c9be87ab71c5
Samed Yumukhov <yumukhovsamed@gmail.com>
2024-11-08 17:32:26 +0300

    widget.pt

---
+++ src/widget.py
@@ -0,0 +1,16 @@
+from masks import get_mask_account, get_mask_card_number
+
+def mask_account_card(number_card: str) -> str:
+    """Функция, которая принимает на вход номер счета и возвращает маску"""
+    str_number_card = str(number_card)
+    return f"**{str_number_card[-4:]}"
+
+
+
+    """Функция, которая принимает на вход номер карты и возвращает её маску"""
+    str_number_card = str(number_card)
+    return f"{str_number_card[:4]} {str_number_card[4:6]}** **** {str_number_card[-4:]}"
+
+def get_date(date: str) ->str:
+    """Функция, которая возвращает дату в формате ДД.ММ.ГГГГ."""
+    return f'{date [8:10]}.{date[5:7]}.{date[0:4]}'
